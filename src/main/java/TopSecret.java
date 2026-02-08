c6c9a093b39937ea100c178a75e07621ad2f8c06
ShelbyBrewer <shelby.brewer.k@gmail.com>
2026-02-08 15:53:12 -0500

    Part A user interface

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -2,4 +2,20 @@
  * Commmand Line Utility
  */
 public class TopSecret {
+    public static void main(String[] args){
+        if (args.length == 0){
+            System.out.println("Listing file list please choose one: ");
+            //display number of files call
+        }
+        else if (args.length == 1){
+            System.out.println("Now printing file " + args[0] + " using default cipher key");
+            //return the deciphered file
+        }
+        else if (args.length == 2){
+            System.out.println("Now printing file " + args[0] + " using the key from file: " + args[1]);
+        }
+        else{
+            System.out.println("Invalid number of arguments");
+        }
+    }
 }
