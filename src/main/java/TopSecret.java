84417aeac65500f15b283f872dc92ca44313606c
ShelbyBrewer <shelby.brewer.k@gmail.com>
2026-02-13 12:22:22 -0500

    Additional error handling so the program will 'exit gracefully'

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -4,23 +4,29 @@ import java.io.FileNotFoundException;
  * Commmand Line Utility
  */
 public class TopSecret {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         FileHandler fileHandler = new FileHandler();
         ProgramControl c = new ProgramControl(fileHandler);
-        if (args.length == 0){
-            System.out.println("Listing file list please choose one: ");
-            System.out.println(c.listFiles());
-        }
-        else if (args.length == 1){
-            System.out.println("Now printing file " + args[0] + " using default cipher key");
-            System.out.println(c.getDecipheredFile(args[0]));
-        }
-        else if (args.length == 2){
-            System.out.println("Now printing file " + args[0] + " using the key from file: " + args[1]);
-            System.out.println(c.getAlternateKeyDecipheredFile(args[0], args[1]));
-        }
-        else{
-            System.out.println("Invalid number of arguments");
+
+        try {
+            if (args.length == 0) {
+                System.out.println("Listing file list please choose one: ");
+                System.out.println(c.listFiles());
+            } else if (args.length == 1) {
+                System.out.println("Now printing file " + args[0] + " using default cipher key");
+                System.out.println(c.getDecipheredFile(args[0]));
+            } else if (args.length == 2) {
+                System.out.println("Now printing file " + args[0] + " using the key from file: " + args[1]);
+                System.out.println(c.getAlternateKeyDecipheredFile(args[0], args[1]));
+            } else {
+                System.out.println("Invalid number of arguments");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR : File not found");
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR : Number too large");
+        } catch (Exception e) {
+            System.out.println("ERROR : general error message, code exhibited unexpected behavior");
         }
     }
 }
