446975676293a309bfb6dcf2eafa20a2e5924e1e
ShelbyBrewer <shelby.brewer.k@gmail.com>
2026-02-12 20:38:46 -0500

    edits to person A

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,18 +1,23 @@
+import java.io.FileNotFoundException;
+
 /**
  * Commmand Line Utility
  */
 public class TopSecret {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
+        FileHandler fileHandler = new FileHandler();
+        ProgramControl c = new ProgramControl(fileHandler);
         if (args.length == 0){
             System.out.println("Listing file list please choose one: ");
-            //display number of files call
+            System.out.println(c.listFiles());
         }
         else if (args.length == 1){
             System.out.println("Now printing file " + args[0] + " using default cipher key");
-            //return the deciphered file
+            System.out.println(c.getDecipheredFile(args[0]));
         }
         else if (args.length == 2){
             System.out.println("Now printing file " + args[0] + " using the key from file: " + args[1]);
+            System.out.println(c.getEncipheredFile(args[0], args[1]));
         }
         else{
             System.out.println("Invalid number of arguments");
